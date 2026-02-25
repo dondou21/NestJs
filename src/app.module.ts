48a222e8250b6cd0a5f017ca413885d9468f4b45
dondou21 <dondouabiyi@gmail.com>
2026-02-25 05:24:29 +0200

    user service  created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
+import { UserController } from './user/user.controller';
 
 @Module({
-  controllers: [],
+  controllers: [UserController],
   providers: [],
   imports: [TasksModule, UsersModule],
 })
